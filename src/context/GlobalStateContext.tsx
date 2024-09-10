993ea14c74428150abf0b4dd062c80340d029d65
unknown <sa.vahidinovin@gmail.com>
2024-09-10 14:49:05 +0200

    Added GlobalState and wrap the router in GlobalSatateProvider in main.tsx

--- src/context/GlobalStateContext.tsx
+++ src/context/GlobalStateContext.tsx
@@ -1,2 +1,26 @@
-// Contexts for global state management
-// Context and types
+import { createContext, useState, ReactNode } from 'react';
+import { Drink } from '../type';
+
+interface GlobalStateContextType {
+  searchResults: Drink[];
+  selectedCocktail: Drink | null;
+  setSearchResults: (drinks: Drink[]) => void;
+  setSelectedCocktail: (drink: Drink) => void;
+}
+
+// Create the context
+const GlobalStateContext = createContext<GlobalStateContextType | undefined>(undefined);
+
+// Create a provider 
+export const GlobalStateProvider = ({ children }: { children: ReactNode }) => {
+  const [searchResults, setSearchResults] = useState<Drink[]>([]);
+  const [selectedCocktail, setSelectedCocktail] = useState<Drink | null>(null);
+
+  return (
+    <GlobalStateContext.Provider value={{ searchResults, selectedCocktail, setSearchResults, setSelectedCocktail }}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export default GlobalStateContext;
