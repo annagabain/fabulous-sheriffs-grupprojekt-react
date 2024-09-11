00185ed7669f94b6e5eb32792e51d2151776e1fb
unknown <sa.vahidinovin@gmail.com>
2024-09-11 17:15:15 +0200

    Fetched and displayed a random cocktail, added landing page style

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,47 @@
+import { useEffect, useState, useContext } from 'react';
+import GlobalStateContext from '../context/GlobalStateContext';
+import { Drink } from '../type';
+import { getRandomCocktail } from '../services/api';
+import CocktailCard from '../components/CocktailCard';
+
 export default function LandingPage() {
+    // State to store the fetched random cocktail
+    const [randomCocktail, setRandomCocktail] = useState<Drink | null>(null);
+
+    // Destructure the global context to use the setSelectedCocktail function
+    const { setSelectedCocktail } = useContext(GlobalStateContext);
+
+    // Function to fetch a random cocktail and set it in the local state
+    const fetchNewRandomCocktail = async () => {
+        try {
+            // Fetch a new random cocktail from the API
+            const cocktail = await getRandomCocktail();
+            // Update the state with the fetched cocktail
+            setRandomCocktail(cocktail);
+        } catch (error) {
+            console.error("Failed to fetch random cocktail:", error);
+        }
+    };
+
+    // useEffect hook to fetch a random cocktail when the component mounts
+    useEffect(() => {
+        fetchNewRandomCocktail(); // Trigger API call to fetch a cocktail on first render
+    }, []);
 
     return (
-        <>
-        LandingPage
-        </>
-    )
+        <section className='random-cocktail-container'>
+            {/* Conditional rendering: to make sure that the cocktail card is only rendered when randomCocktail is not null */}
+            {randomCocktail && (
+                <>
+                    <section className='cocktail-card'>
+                        <CocktailCard drink={randomCocktail} />
+                        <button onClick={() => setSelectedCocktail(randomCocktail)} className='view-more'>
+                            View Details
+                        </button>
+                        <button onClick={fetchNewRandomCocktail} className='show-another-cocktail'>Show Another Cocktail</button>
+                    </section>
+                </>
+            )}
+        </section>
+    );
 }
