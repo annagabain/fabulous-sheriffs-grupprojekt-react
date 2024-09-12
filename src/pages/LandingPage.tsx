ef36a2653f0199c8ef24768835a0624de2260f32
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-09-12 09:25:17 +0200

    19 creating landing page (#52)
    * Added CocktailCard
    
    * Fetched and displayed a random cocktail, added landing page style
    
    * Added handleViewDetails function

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,60 @@
+import { useEffect, useState, useContext } from 'react';
+import GlobalStateContext from '../context/GlobalStateContext';
+import { Drink } from '../type';
+import { getRandomCocktail } from '../services/api';
+import CocktailCard from '../components/CocktailCard';
+import { useNavigate } from 'react-router-dom';
+
 export default function LandingPage() {
+    // State to store the fetched random cocktail
+    const [randomCocktail, setRandomCocktail] = useState<Drink | null>(null);
+
+    // Destructure the global context to use the setSelectedCocktail function
+    const { setSelectedCocktail } = useContext(GlobalStateContext);
+
+    // Hook to handle navigation
+    const navigate = useNavigate(); 
+
+    // Function to fetch a random cocktail and set it in the local state
+    const fetchNewRandomCocktail = async () => {
+        try {
+            // Fetch a new random cocktail from the API
+            const cocktail = await getRandomCocktail();
+            // Update the state with the fetched cocktail
+            setRandomCocktail(cocktail);
+        } catch (error) {
+            console.error("Failed to fetch random cocktail:", error);
+        }
+    };
+
+    // useEffect hook to fetch a random cocktail when the component mounts
+    useEffect(() => {
+        fetchNewRandomCocktail(); // Trigger API call to fetch a cocktail on first render
+    }, []);
+
+    const handleViewDetails = () => {
+        if (randomCocktail) {
+            // Set the selected cocktail in global state
+            setSelectedCocktail(randomCocktail); 
+            // Navigate to the details page using the cocktail's ID
+            navigate(`/cocktail/${randomCocktail.idDrink}`);
+        }
+    };
 
     return (
-        <>
-        LandingPage
-        </>
-    )
+        <section className='random-cocktail-container'>
+            {/* Conditional rendering: to make sure that the cocktail card is only rendered when randomCocktail is not null */}
+            {randomCocktail && (
+                <>
+                    <section className='cocktail-card'>
+                        <CocktailCard drink={randomCocktail} />
+                        <button onClick={handleViewDetails} className='view-more'>
+                            View Details
+                        </button>
+                        <button onClick={fetchNewRandomCocktail} className='show-another-cocktail'>Show Another Cocktail</button>
+                    </section>
+                </>
+            )}
+        </section>
+    );
 }
