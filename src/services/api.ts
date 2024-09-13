818a0d6a031475243cced425317b465cab73dfca
Isa Par <isabelle826@hotmail.com>
2024-09-13 15:22:23 +0200

    Updated api.ts to use interface Drink

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,14 @@
+import { Drink } from "../type";
+
 export const getCocktailByName = async (name: string) => {
     try {
         const response = await fetch(
             `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
         );
         const data = await response.json();
-        console.log(data.drinks);
+        const drinks: Drink = data.drinks;
 
-        return data.drinks;
+        return drinks;
     } catch (error) {
         console.error("Error fetching cocktail by name:", error);
         throw error;
@@ -19,26 +21,26 @@ export const getRandomCocktail = async () => {
             "https://www.thecocktaildb.com/api/json/v1/1/random.php"
         );
         const data = await response.json();
-        console.log(data.drinks[0]);
+        const drink: Drink = data.drinks[0];
 
-        return data.drinks[0];
+        return drink;
     } catch (error) {
         console.error("Error fetching random cocktail:", error);
         throw error;
     }
 };
 
-export const getCocktailDetails = async (idDrink: number) => {
-    try {
-        const response = await fetch(
-            `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`
-        );
-        const data = await response.json();
-        console.log(data.drinks[0]);
+// export const getCocktailDetails = async (idDrink: number) => {
+//     try {
+//         const response = await fetch(
+//             `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`
+//         );
+//         const data = await response.json();
+//         console.log(data.drinks[0]);
 
-        return data.drinks[0];
-    } catch (error) {
-        console.error("Error fetching random cocktail:", error);
-        throw error;
-    }
-};
+//         return data.drinks[0];
+//     } catch (error) {
+//         console.error("Error fetching random cocktail:", error);
+//         throw error;
+//     }
+// };
