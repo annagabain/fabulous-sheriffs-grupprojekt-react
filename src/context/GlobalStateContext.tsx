45a7d982b382b5f477f6a647b9ef918d22532bce
unknown <sa.vahidinovin@gmail.com>
2024-09-16 13:29:16 +0200

    Added favorites global context

--- src/context/GlobalStateContext.tsx
+++ src/context/GlobalStateContext.tsx
@@ -6,12 +6,16 @@ interface GlobalStateContextType {
   selectedCocktail: Drink | null;
   setSearchResults: (drinks: Drink[]) => void;
   setSelectedCocktail: (drink: Drink) => void;
+  favorites: Drink[];
+  setFavorites: (drinks: Drink[]) => void;
 }
 const defaultState: GlobalStateContextType = {
   searchResults: [],
   selectedCocktail: null,
   setSearchResults: () => {},
   setSelectedCocktail: () => {},
+  favorites: [],
+  setFavorites: () => {},
 };
 // Create the context
 
@@ -22,9 +26,10 @@ const GlobalStateContext = createContext<GlobalStateContextType>(defaultState);
 export const GlobalStateProvider = ({ children }: { children: ReactNode }) => {
   const [searchResults, setSearchResults] = useState<Drink[]>([]);
   const [selectedCocktail, setSelectedCocktail] = useState<Drink | null>(null);
+  const [favorites, setFavorites] = useState<Drink[]>([]);
 
   return (
-    <GlobalStateContext.Provider value={{ searchResults, selectedCocktail, setSearchResults, setSelectedCocktail }}>
+    <GlobalStateContext.Provider value={{ searchResults, selectedCocktail, setSearchResults, setSelectedCocktail, favorites, setFavorites }}>
       {children}
     </GlobalStateContext.Provider>
   );
