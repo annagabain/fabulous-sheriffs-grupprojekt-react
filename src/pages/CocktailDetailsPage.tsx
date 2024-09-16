0e509e0e581ceee3f0c932ac5b9ec11eb6a4b4ec
Isa Par <isabelle826@hotmail.com>
2024-09-16 11:25:50 +0200

    34 cocktail details page

--- src/pages/CocktailDetailsPage.tsx
+++ src/pages/CocktailDetailsPage.tsx
@@ -1,7 +1,51 @@
+import GlobalStateContext from "../context/GlobalStateContext";
+import { useContext } from "react";
+import CocktailCard from "../components/CocktailCard";
+import { Drink } from "../type";
+
 export default function CocktailDetailsPage() {
+    const { selectedCocktail } = useContext(GlobalStateContext);
+    if (!selectedCocktail) return;
+
+    const ingredients = Object.keys(selectedCocktail)
+        .filter(
+            (key) =>
+                key.startsWith("strIngredient") &&
+                selectedCocktail[key as keyof Drink]
+        )
+        .map((key) => ({
+            ingredient: selectedCocktail[key as keyof Drink],
+            measure: selectedCocktail[`strMeasure${key.slice(13)}` as keyof Drink],
+        }));
+
     return (
         <>
-            <h2>Cocktail details page</h2>
+            <h2 className="title">{selectedCocktail.strDrink}</h2>
+            <article className="cocktail-details">
+                <CocktailCard drink={selectedCocktail} />
+                <div className="details-text">
+                    <div className="details-tags">
+                        <p>{selectedCocktail.strCategory}</p>
+                        <p>{selectedCocktail.strTags?.split(",").join(", ")}</p>
+                        <p>{selectedCocktail.strGlass}</p>
+                        <p>{selectedCocktail.strAlcoholic}</p>
+                    </div>
+                    <section className="ingredients">
+                        <h3>Ingredients</h3>
+                        {ingredients.map((ing, i) => {
+                            return (
+                                <p key={i}>
+                                    {ing.ingredient}, {ing.measure}
+                                </p>
+                            );
+                        })}
+                    </section>
+                    <section className="instructions">
+                        <h3>Instructions</h3>
+                        <p>{selectedCocktail.strInstructions}</p>
+                    </section>
+                </div>
+            </article>
         </>
     );
 }
