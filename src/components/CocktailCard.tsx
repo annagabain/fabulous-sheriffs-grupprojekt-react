13fed3117f93fe330c083f1b9be8abcc2cfb46b7
unknown <sa.vahidinovin@gmail.com>
2024-09-17 12:02:05 +0200

    Render favorite list, fixed navigating to cocktail details when clicking on favorite icon

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -2,39 +2,42 @@ import { Drink } from '../type';
 import GlobalStateContext from '../context/GlobalStateContext';
 import { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'; 
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function CocktailCard({ drink }: { drink: Drink }) {
     // Destructure the global context to use the setFavorites function
-    const {favorites, setFavorites} =useContext(GlobalStateContext);
+    const { favorites, setFavorites } = useContext(GlobalStateContext);
 
     // Check if the current drink is already in the list of favorites
     const isFavorite = favorites.some(favorite => favorite.idDrink === drink.idDrink)
 
     // Add the current drink to the favorites list
-    function addFavorite(){
+    function addFavorite() {
         setFavorites([...favorites, drink]);
     }
 
     // Remove the current drink from the favorites list
-    function removeFavorite(){
-        setFavorites(favorites.filter(favorite => favorite.idDrink !== drink.idDrink ))
+    function removeFavorite() {
+        setFavorites(favorites.filter(favorite => favorite.idDrink !== drink.idDrink))
     }
 
     // Toggle between adding and removing the drink from favorites
-    function handlefavoriteClick() {
+    function handlefavoriteClick(e: React.MouseEvent<HTMLButtonElement>) {
+
+        e.stopPropagation();  // Prevent event from bubbling up
+
         if (isFavorite) {
             removeFavorite();  // If already a favorite, remove it
         } else {
             addFavorite();     // If not a favorite, add it
         }
     }
-    
+
     return (
         <div className="card">
             <button className="favorite-icon" onClick={handlefavoriteClick}>
-            <FontAwesomeIcon icon={faHeartSolid} style={{ color: isFavorite ? 'red' : 'white' }} />
+                <FontAwesomeIcon icon={faHeartSolid} style={{ color: isFavorite ? 'red' : 'white' }} />
             </button>
             <img src={drink.strDrinkThumb} alt={drink.strDrink} className="card-image" />
             <h3>{drink.strDrink}</h3>
