14ab17b0a46ae54414a92e9e0d09382fc6e56b7b
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-09-17 10:27:55 +0200

    Add and remove favorite functionality (#70)

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,8 +1,41 @@
 import { Drink } from '../type';
+import GlobalStateContext from '../context/GlobalStateContext';
+import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'; 
+
 
 export default function CocktailCard({ drink }: { drink: Drink }) {
+    // Destructure the global context to use the setFavorites function
+    const {favorites, setFavorites} =useContext(GlobalStateContext);
+
+    // Check if the current drink is already in the list of favorites
+    const isFavorite = favorites.some(favorite => favorite.idDrink === drink.idDrink)
+
+    // Add the current drink to the favorites list
+    function addFavorite(){
+        setFavorites([...favorites, drink]);
+    }
+
+    // Remove the current drink from the favorites list
+    function removeFavorite(){
+        setFavorites(favorites.filter(favorite => favorite.idDrink !== drink.idDrink ))
+    }
+
+    // Toggle between adding and removing the drink from favorites
+    function handlefavoriteClick() {
+        if (isFavorite) {
+            removeFavorite();  // If already a favorite, remove it
+        } else {
+            addFavorite();     // If not a favorite, add it
+        }
+    }
+    
     return (
         <div className="card">
+            <button className="favorite-icon" onClick={handlefavoriteClick}>
+            <FontAwesomeIcon icon={faHeartSolid} style={{ color: isFavorite ? 'red' : 'white' }} />
+            </button>
             <img src={drink.strDrinkThumb} alt={drink.strDrink} className="card-image" />
             <h3>{drink.strDrink}</h3>
             {/* <p>{drink.strInstructions}</p> */}
