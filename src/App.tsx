6c87584f1632bafd0fa2ef8cacdaa1452448e744
AG <anna.gabain@gmail.com>
2024-09-26 15:39:48 +0200

    Footer added, along with some global style changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer"; 
 
 export default function App() {
     // const [count, setCount] = useState(0)
@@ -10,6 +11,8 @@ export default function App() {
         <>
             <Header />
             <Outlet />
+            <Footer />
+
         </>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>''Fabulous Sheriffs'' for Lexicon ©2024.</p>
+        </footer>
+    );
+};
+
+export default Footer;
