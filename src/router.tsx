e2854acb6472db8eba954576bb7ba6cda52f4ef2
AG <anna.gabain@gmail.com>
2024-09-27 14:15:44 +0200

    Switched to HashRouter for GitHub Pages deployment

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { createHashRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
 import SearchPage from "./pages/SearchPage";
 import LandingPage from "./pages/LandingPage";
@@ -7,15 +7,15 @@ import CocktailDetailsPage from "./pages/CocktailDetailsPage";
 import PageNotFound from "./pages/PageNotFound";
 import IngredientDetailsPage from "./pages/IngredientDetailsPage";
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
            <Route index element={<LandingPage />} />
-           <Route path='search-page' element={<SearchPage />} />
-           <Route path='favorites-page' element={<FavoritesPage />} />
-           <Route path='cocktail/:id' element={<CocktailDetailsPage />} />
-           <Route path='ingredient/:ingredient' element={<IngredientDetailsPage />} />
-           <Route path='*' element={<PageNotFound />} />
+           <Route path="search-page" element={<SearchPage />} />
+           <Route path="favorites-page" element={<FavoritesPage />} />
+           <Route path="cocktail/:id" element={<CocktailDetailsPage />} />
+           <Route path="ingredient/:ingredient" element={<IngredientDetailsPage />} />
+           <Route path="*" element={<PageNotFound />} />
         </Route>
     )
 );
